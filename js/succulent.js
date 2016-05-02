c6ff00606416d82e0c56fc58ed123beeb604a65a
Tyler Powers <gr4yscale@gmail.com>
2016-05-02 20:00:24 +0100

    Cleanup; junk commit

--- js/succulent.js
+++ js/succulent.js
@@ -7,7 +7,6 @@ module.exports = function(THREE) {
   }
 
   return function() {
-    var petals = [];
     var petalCount = Math.floor(getRandomArbitrary(20, 40));
     var curveAmountA;
     var curveAmountB = getRandomArbitrary(0.08, 0.20); // multiplier for log curvature
@@ -53,8 +52,6 @@ module.exports = function(THREE) {
       return mesh;
     }
 
-    var boxGeometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
-    var boxMesh = new THREE.Mesh(boxGeometry, shaderMaterial);
     var singleGeometry = new THREE.Geometry();
 
     for (var i = 1; i < petalCount; i++) {
@@ -67,22 +64,18 @@ module.exports = function(THREE) {
       //curveAmount = 0.1 + (Math.pow(j, 2.0) * 1.000001);
 
       var petalMesh = createPetalMesh();
-
       petalMesh.rotation.y = THREE.Math.degToRad(rotationAmount * 360);
 
       var scale = curveAmountA;
       petalMesh.scale.x = scale;
       petalMesh.scale.y = scale;
       petalMesh.scale.z = scale;
-      petals.push();
 
       petalMesh.updateMatrix();
       singleGeometry.merge(petalMesh.geometry, petalMesh.matrix);
-
-      // sphereMesh.add(petalMesh);
     }
 
-    singleMesh = new THREE.Mesh(singleGeometry, shaderMaterial);
+    var singleMesh = new THREE.Mesh(singleGeometry, shaderMaterial);
 
     return singleMesh;
   }
