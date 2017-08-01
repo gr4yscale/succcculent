7ed39ff5473a9a7310e8adfd4df9767702d22fc3
Tyler Powers <tyler@unstatic.tech>
2017-08-01 03:25:13 +0100

    move index.js into the js folder

--- js/index.js
+++ js/index.js
@@ -2,18 +2,18 @@ var domReady = require('domready');
 var dat = require('dat-gui');
 var glslify = require('glslify');
 
-require('./js/FileSaver.js')
+require('./FileSaver.js')
 
 // TOFIX: changed this to find the correct THREE reference
-const stlExporter = require('three-STLexporter') // adds to the global THREE namespace
+// const stlExporter = require('three-STLexporter') // adds to the global THREE namespace
 
 let WebMidi = require('webmidi')
-var Succulent = require('./js/succulent')(THREE);
-var Presets = require('./js/presets')
-var Controls = require('./js/controls')
-let state = require('./js/state.js')
-let events = require('./js/events.js')
-let config = require('./js/config.js')
+var Succulent = require('./succulent')(THREE);
+var Presets = require('./presets')
+var Controls = require('./controls')
+let state = require('./state.js')
+let events = require('./events.js')
+let config = require('./config.js')
 
 var camera, scene, renderer;
 var container;
@@ -31,6 +31,56 @@ var shaders = [];
 var fragShaders = [];
 
 var controls;
+
+
+
+
+
+
+
+
+var composer, renderPass, effectCopy;
+var bleachFilter, edgeShader, FXAAShader, focusShader, colorCorrectionShader, hueSaturationShader
+
+var controlsGUI = new function () {
+    this.bleachOpacity = 1;
+    this.bleach = false;
+    this.edgeDetect = false;
+    this.edgeAspect = 512;
+    this.FXAA = false;
+    this.focus = false;
+    this.sampleDistance = 0.94;
+    this.waveFactor = 0.00125;
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+
+    this.colorR = 1;
+    this.colorG = 1;
+    this.colorB = 1;
+
+    this.hue = 0;
+    this.saturation = 0;
+
+    this.onChange = function () {
+        bleachFilter.enabled = controlsGUI.bleach;
+        bleachFilter.uniforms.opacity.value = controlsGUI.bleachOpacity;
+        edgeShader.enabled = controlsGUI.edgeDetect;
+        edgeShader.uniforms.aspect.value = new THREE.Vector2(controlsGUI.edgeAspect, controlsGUI.edgeAspect);
+        FXAAShader.enabled = controlsGUI.FXAA;
+        FXAAShader.uniforms.resolution.value = new THREE.Vector2(1 / window.innerWidth, 1 / window.innerHeight);
+        focusShader.enabled = controlsGUI.focus;
+        focusShader.uniforms.screenWidth.value = controlsGUI.screenWidth;
+        focusShader.uniforms.screenHeight.value = controlsGUI.screenHeight;
+        focusShader.uniforms.waveFactor.value = controlsGUI.waveFactor;
+        focusShader.uniforms.sampleDistance.value = controlsGUI.sampleDistance;
+
+        // colorCorrectionShader.uniforms.mulRGB.value.set(controlsGUI.colorR, controlsGUI.colorG, controlsGUI.colorB)
+        hueSaturationShader.uniforms.hue.value = controlsGUI.hue
+        hueSaturationShader.uniforms.saturation.value = controlsGUI.saturation
+    }
+};
+
+
 
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
@@ -93,6 +143,55 @@ function addSucculent(index, plantParams) {
     } else {
       positionSucculentRandomly(index, plantParams, succulent)
     }
+
+    /////////////////////////////////
+
+    // look at BufferGeometry .setFromObject
+
+    // This is based on the SUCCULENT position, not petal mesh - so y component will be useless
+
+    // The succulent mesh is actually empty and just contains children petal meshes.
+    // We need to add a BufferAttribute to the petal geometries
+    // to get the plant position attribute in the vertex shader
+
+    for (var m = 0; m < succulent.children.length; m++) {
+      let petalMesh = succulent.children[m]
+
+      let plantPositionAttributeArray = new Float32Array(petalMesh.geometry.attributes.position.count * 3)
+
+      // lay out the position data in the way that the GPU wants it.
+      // we deal with it on the CPU like this: [ [x,y,z], [x,y,z], [x,y,z]] ...
+      // the gpu wants this: [ x, y, z, x, y, z, x, y, z, x, y, z ]
+
+      for (var i = 0; i < plantPositionAttributeArray.length; i++) {
+        plantPositionAttributeArray[i * 3] = succulent.position.x
+        plantPositionAttributeArray[i * 3 + 1] = succulent.position.y
+        plantPositionAttributeArray[i * 3 + 2] = succulent.position.z
+      }
+
+      let petalMeshRotationAttributeArray = new Float32Array(petalMesh.geometry.attributes.position.count)
+      for (var j = 0; j < petalMeshRotationAttributeArray.length; j++) {
+        petalMeshRotationAttributeArray[j] = petalMesh.rotation.y
+      }
+
+
+      // console.log('petalMesh geom position attributes:')
+      // console.log(petalMesh.geometry.attributes.position)
+      let posAttribs = petalMesh.geometry.attributes.position.array
+      let l = petalMesh.geometry.attributes.position.count
+      // console.log(posAttribs[0] + ' | ' + posAttribs[1] + ' | ' + posAttribs[2])
+      // console.log(posAttribs[0] + ' | ' + posAttribs[1] + ' | ' + posAttribs[2])
+      // console.log(posAttribs[0] + ' | ' + posAttribs[1] + ' | ' + posAttribs[2])
+      // console.log(posAttribs[l-30] + ' | ' + posAttribs[l-29] + ' | ' + posAttribs[l-28 ])
+
+      // console.log('plant position attributes array:')
+      // console.log(plantPositionAttributeArray[0] + ' | ' + plantPositionAttributeArray[1] + ' | ' + plantPositionAttributeArray[2])
+      // debugger
+
+      petalMesh.geometry.addAttribute("plantPosition", new THREE.BufferAttribute(plantPositionAttributeArray, 3))
+      petalMesh.geometry.addAttribute("petalRotation", new THREE.BufferAttribute(petalMeshRotationAttributeArray, 1))
+    }
+
     scene.add(succulent);
     succulents.push(succulent);
   }
@@ -108,20 +207,25 @@ function addSucculent(index, plantParams) {
 function loadShaderMaterials() {
   // this makes use of the browserify transform (it's a preprocessor)
   // so can't clean this up with a loop
-  fragShaders.push(glslify(__dirname + '/shaders/1.frag'))
-  fragShaders.push(glslify(__dirname + '/shaders/2.frag'))
-  fragShaders.push(glslify(__dirname + '/shaders/3.frag'))
-  fragShaders.push(glslify(__dirname + '/shaders/4.frag'))
-  fragShaders.push(glslify(__dirname + '/shaders/5.frag'))
-  fragShaders.push(glslify(__dirname + '/shaders/6.frag'))
-  fragShaders.push(glslify(__dirname + '/shaders/7.frag'))
-  fragShaders.push(glslify(__dirname + '/shaders/8.frag'))
-  fragShaders.push(glslify(__dirname + '/shaders/9.frag'))
-  fragShaders.push(glslify(__dirname + '/shaders/10.frag'))
-  fragShaders.push(glslify(__dirname + '/shaders/11.frag'))
-  fragShaders.push(glslify(__dirname + '/shaders/12.frag'))
-  fragShaders.push(glslify(__dirname + '/shaders/13.frag'))
-  fragShaders.push(glslify(__dirname + '/shaders/14.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/1.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/2.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/3.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/4.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/5.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/6.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/7.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/8.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/9.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/10.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/11.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/12.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/13.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/14.frag'))
+  fragShaders.push(glslify(__dirname + './../shaders/15.frag'))
+  // fragShaders.push(glslify(__dirname + '/shaders/16.frag'))
+  // fragShaders.push(glslify(__dirname + '/shaders/17.frag'))
+  // fragShaders.push(glslify(__dirname + '/shaders/18.frag'))
+  // fragShaders.push(glslify(__dirname + '/shaders/19.frag'))
 
   for (var i = 0; i < fragShaders.length; i++) {
     var shaderMaterial = new THREE.ShaderMaterial({
@@ -134,10 +238,12 @@ function loadShaderMaterials() {
       defines: {
         USE_MAP: ''
       },
-      vertexShader : glslify(__dirname + '/shaders/passthrough.vert'),
+      vertexShader : glslify(__dirname + './../shaders/passthrough.vert'),
       fragmentShader : fragShaders[i],
       side: THREE.DoubleSide,
-      // wireframe: true
+      // transparent: false,
+      blending: THREE.AdditiveBlending,
+      wireframe:false
     });
     shaders.push(shaderMaterial);
   }
@@ -177,6 +283,26 @@ domReady(function(){
   guiGardenGen.add(state, 'adHocPlantParamsCurveAmountC', 0.01, 3.0).step(0.01).name("Curve Amt C").onFinishChange(regenGardenIfAllowed)
   guiGardenGen.add(state, 'adHocPlantParamsCurveAmountD', 0.01, 3.0).step(0.01).name("Curve Amt D").onFinishChange(regenGardenIfAllowed)
   guiGardenGen.add(state, 'adHocPlantParamsLayers', 2, 40).step(1).name("# Layers").onFinishChange(regenGardenIfAllowed)
+
+
+
+  gui.add(controlsGUI, 'bleach').onChange(controlsGUI.onChange);
+  gui.add(controlsGUI, 'bleachOpacity', 0, 2).onChange(controlsGUI.onChange);
+  gui.add(controlsGUI, 'edgeDetect').onChange(controlsGUI.onChange);
+  gui.add(controlsGUI, 'edgeAspect', 128, 2048).step(128).onChange(controlsGUI.onChange);
+  gui.add(controlsGUI, 'FXAA').onChange(controlsGUI.onChange);
+  gui.add(controlsGUI, 'focus').onChange(controlsGUI.onChange);
+  gui.add(controlsGUI, 'sampleDistance', 0, 2).step(0.01).onChange(controlsGUI.onChange);
+  gui.add(controlsGUI, 'waveFactor', 0, 0.005).step(0.0001).onChange(controlsGUI.onChange);
+
+  gui.add(controlsGUI, 'colorR', 0, 1).onChange(controlsGUI.onChange);
+  gui.add(controlsGUI, 'colorG', 0, 1).onChange(controlsGUI.onChange);
+  gui.add(controlsGUI, 'colorB', 0, 1).onChange(controlsGUI.onChange);
+
+  gui.add(controlsGUI, 'hue', -1, 1).onChange(controlsGUI.onChange);
+  gui.add(controlsGUI, 'saturation', -1, 1).onChange(controlsGUI.onChange);
+
+
 
   // dat.GUI.toggleHide()
 
@@ -223,11 +349,22 @@ function generateNewRandomGarden() {
 }
 
 function initThree() {
-  renderer = new THREE.WebGLRenderer({'antialias': true, alpha: false, precision: 'highp'});
+  renderer = new THREE.WebGLRenderer({'antialias': false, alpha: false, precision: 'highp'});
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setClearColor(new THREE.Color(0x333333, 1.0));
-  renderer.setFaceCulling(false)
+
+
+
+  // **** FUCKS UP POST PROCESSING TO HAVE FACE CULLING FALSE
+
+
+  // renderer.setFaceCulling(false)
+
+
+
+  // ******************************** !!!!!
+
 
   container = document.getElementById('webglrender');
   container.appendChild(renderer.domElement);
@@ -242,9 +379,37 @@ function initThree() {
   controls = new Controls(presets, state, WebMidi, scene, camera, renderer.domElement, handleControlsEvent)
 
   // setup the scene
-  var setupLights = require('./js/lights')(THREE, scene);
+  var setupLights = require('./lights')(THREE, scene);
   setupLights();
   loadShaderMaterials();
+
+  // create the shaders
+  // overlay of black and white
+  bleachFilter = new THREE.ShaderPass(THREE.BleachBypassShader);
+  bleachFilter.enabled = false;
+  edgeShader = new THREE.ShaderPass(THREE.EdgeShader);
+  edgeShader.enabled = false;
+  FXAAShader = new THREE.ShaderPass(THREE.FXAAShader);
+  FXAAShader.enabled = false;
+  focusShader = new THREE.ShaderPass(THREE.FocusShader);
+  focusShader.enabled = false;
+  colorCorrectionShader = new THREE.ShaderPass(THREE.ColorCorrectionShader);
+  hueSaturationShader = new THREE.ShaderPass(THREE.HueSaturationShader);
+
+  renderPass = new THREE.RenderPass(scene, camera);
+  effectCopy = new THREE.ShaderPass(THREE.CopyShader);
+  effectCopy.renderToScreen = true;
+
+  composer = new THREE.EffectComposer(renderer);
+
+  composer.addPass(renderPass);
+  composer.addPass(bleachFilter);
+  composer.addPass(edgeShader);
+  composer.addPass(FXAAShader);
+  composer.addPass(focusShader);
+  composer.addPass(hueSaturationShader);
+
+  composer.addPass(effectCopy);
 
   window.addEventListener('resize', resize, false);
   setTimeout(resize, 1);
@@ -295,14 +460,15 @@ function updateShaderUniforms(t) {
 }
 
 function update(t) {
-  updateSucculentTextures()
+  // updateSucculentTextures()
   updateShaderUniforms(t)
   resize()
   controls.update(state, presets)
 }
 
 function render(dt) {
-  renderer.render(scene, camera);
+  //renderer.render(scene, camera);
+  composer.render();
 }
 
 function animate(t) {
