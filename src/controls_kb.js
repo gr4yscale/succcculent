071877a2ababe56f02ace9fa90242ffcce4e73f2
Tyler Powers <gr4yscale@gmail.com>
2018-08-21 16:07:33 +0200

    drop in old controls files from another project

---
+++ src/controls_kb.js
@@ -0,0 +1,12 @@
+let events = require('./events.js')
+var kb = exports
+
+kb.buttonIdentifierToEventIdentifier = {
+  '/': events.CAMERA_PRESETS_LEARN_TOGGLED,
+  '.': events.XBOX_CONTROLLER_SELECTION_TOGGLED,
+  ',': events.GENERATE_NEW_RANDOM_GARDEN,
+  'm': events.GENERATE_NEW_PLANTS_TEXTURE_STYLES_TOGGLE,
+  'n': events.CAMERA_CONTROLS_RESET,
+  's': events.SAVE_GARDEN_TO_PRESET_FILE,
+  'q': events.LOAD_GARDEN_FROM_PRESET_FILE
+}
---
+++ src/controls_midi_touchosc.js
@@ -0,0 +1,19 @@
+let events = require('./events.js')
+var touchOSC = exports
+
+// TOFIX: devise a scheme to maintain visual state of touchOSC40 grid based on underlying state (presets, controls, etc)
+
+// constants
+
+touchOSC.buttonIdentifierToEventIdentifier = {
+  'touchOSC-E1': events.CAMERA_PRESETS_LEARN_TOGGLED,
+  'touchOSC-F1': events.XBOX_CONTROLLER_SELECTION_TOGGLED,
+  'touchOSC-D#1': events.CAMERA_CONTROLS_RESET,
+  'touchOSC-C3': events.GENERATE_NEW_PLANTS_TEXTURE_STYLES_TOGGLE
+  // 'B1': events.GENERATE_NEW_RANDOM_GARDEN,
+  // 'C1': events.GENERATE_NEW_PLANTS_TEXTURE_STYLES_TOGGLE,
+}
+
+
+// TODO:
+// Map knob values to variables?
